90cfc7ffdfcf1bd6552101fde731c5ba9b4f4c73
who <fj8a79wehf89@yahoo.com>
2023-05-14 13:55:55 +0800

    5.py

---
+++ tests/5.py
@@ -0,0 +1,9 @@
+"""
+https://python.langchain.com/en/latest/modules/memory/examples/agent_with_memory.html
+
+
+这两个大约是和ctx length有关的
+ConversationTokenBufferMemory
+ConversationSummaryBufferMemory
+而autogpt中显然内嵌了一个summary机制
+"""
